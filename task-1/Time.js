7e4cac388811da5bbafda64a1a6d3497a1e99d05
Mustafa Homsi <homsi6313@gmail.com>
2026-03-25 20:44:39 +0100

    t1 w11 done

--- task-1/Time.js
+++ task-1/Time.js
@@ -1,3 +1,38 @@
-export class Time {
-  // Your code here
+// Time.js
+class Time {
+  constructor(h = 0, m = 0, s = 0) {
+    this.hours = h;
+    this.minutes = m;
+    this.seconds = s;
+  }
+
+  getHours() { return this.hours; }
+  getMinutes() { return this.minutes; }
+  getSeconds() { return this.seconds; }
+
+  addHours(h) {
+    this.hours = (this.hours + h) % 24;
+    if (this.hours < 0) this.hours += 24;
+  }
+
+  addMinutes(m) {
+    const totalMinutes = this.minutes + m;
+    this.addHours(Math.floor(totalMinutes / 60));
+    this.minutes = ((totalMinutes % 60) + 60) % 60;
+  }
+
+  addSeconds(s) {
+    const totalSeconds = this.seconds + s;
+    this.addMinutes(Math.floor(totalSeconds / 60));
+    this.seconds = ((totalSeconds % 60) + 60) % 60;
+  }
+
+  toString() {
+    const h = String(this.getHours()).padStart(2, '0');
+    const m = String(this.getMinutes()).padStart(2, '0');
+    const s = String(this.getSeconds()).padStart(2, '0');
+    return `${h}:${m}:${s}`;
+  }
 }
+
+module.exports = { Time };
--- task-1/index.js
+++ task-1/index.js
@@ -1,4 +1,14 @@
-import { Time } from './Time.js';
+const Time = require('./task-1/Time');
 
-const time = new Time(13, 37, 0);
-console.log(time.toString()); // Output: "13:37:00"
+const myTime = new Time(12, 35, 0);
+
+console.log(myTime.toString()); // 12:35:00
+
+myTime.addMinutes(25);
+console.log(myTime.toString()); // 13:00:00
+
+myTime.addHours(12);
+console.log(myTime.toString()); // 01:00:00
+
+myTime.addSeconds(-3600);
+console.log(myTime.toString()); // 00:00:00
